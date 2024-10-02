b1d0f4591da5ae5bbcc45c47a3aa5da6271e91e7
ValentinMxC0de <valentinogu417@gmail.com>
2024-10-02 09:59:43 -0300

    Update MainActivity3.java

--- app/src/main/java/com/example/evaluacion1vmx/MainActivity3.java
+++ app/src/main/java/com/example/evaluacion1vmx/MainActivity3.java
@@ -11,21 +11,21 @@ public class MainActivity3 extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        // Mantén esta línea para instalar el splash screen
+        
         SplashScreen splashScreen = SplashScreen.installSplashScreen(this);
 
         super.onCreate(savedInstanceState);
 
-        // Asegúrate de que estás inflando el layout correcto para MainActivity3
-        setContentView(R.layout.activity_main3); // Cambia 'activity_main3' al nombre real de tu layout
+        
+        setContentView(R.layout.activity_main3); 
 
-        // Retraso para simular la carga de la aplicación (puedes ajustar la duración)
+        
         new Handler(Looper.getMainLooper()).postDelayed(() -> {
-            // Inicia MainActivity2 después del retraso
+            
             Intent intent = new Intent(MainActivity3.this, MainActivity2.class);
             startActivity(intent);
 
-            // Finaliza MainActivity3 para que el usuario no pueda volver atrás presionando el botón de retroceso
+          
             finish();
         }, 0);
     }
